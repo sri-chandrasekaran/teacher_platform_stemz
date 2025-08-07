a4850dec67c63a2027f2992a96c993bf6beb9edb
jingus-bingus <jholmes9@ucmerced.edu>
2025-08-07 11:01:04 -0700

    modified:   src/apiService.js

--- src/apiService.js
+++ src/apiService.js
@@ -1,5 +1,5 @@
-// const BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:3000/api';
-const BASE_URL = 'http://localhost:3000/api'; // Default to local API for development
+const BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:3000/api';
+// const BASE_URL = 'http://localhost:3000/api'; // Default to local API for development
 
 class ApiService {
     // User-related API calls
