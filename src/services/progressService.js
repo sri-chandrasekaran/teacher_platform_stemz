ea4000b1af78d46667aadde6d251fd9086ae6005
Drishti Regmi <dregmi@ucsd.edu>
2025-08-13 13:37:50 -0700

    t Revert "testing something"
    This reverts commit 2f8d9d00fe6b289a0e579c3d89b8244f3adb5081.

--- src/services/progressService.js
+++ src/services/progressService.js
@@ -15,4 +15,4 @@ export const getUserCourseProgress = async (courseName, userId) => {
   }
 };
 
-getUserCourseProgress('astronomy', '688d6519ab0d28950abd0321').then(console.log).catch(console.error);
+getUserCourseProgress('Astronomy', '688d6519ab0d28950abd0321').then(console.log).catch(console.error);
