e45914cdf6f5543d872b99a9bda2f44d88392ad0
sri-chandrasekaran <122851944+sri-chandrasekaran@users.noreply.github.com>
2025-08-13 21:06:04 -0700

    add state

--- src/components/ClassroomCard.js
+++ src/components/ClassroomCard.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import AddStudentModal from '../components/AddStudentModal';
 
 const ClassroomCard = ({ classroom, onEnter, onDelete, onEdit, onInvite, students = [], onStudentAdded }) => {
